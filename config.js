8b934bcc1de57c43959e23da8f073cda9b1df1cb
Grant <103982738+RainGrant@users.noreply.github.com>
2022-04-19 13:37:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
@@ -30,7 +29,7 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
+        bannar_coming: "氛围",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
