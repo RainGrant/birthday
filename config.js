4d47801e83fa1d74355f44b7d20d4ae040031de5
Grant <103982738+RainGrant@users.noreply.github.com>
2022-04-20 13:11:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ var config = {
      */
     imgs: {
         "我的好朋友": "./birthdayphoto.jpg",
-        "平安健康 心想事成!": "./birthdayphoto.jpg",
+        "平安健康 心想事成!": "./birthdayphoto1.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
